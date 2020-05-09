6d37a760d7da60b5328497aeeecb881b290f6aa5
marcospedro97 <marcos.pedro.nunes@gmail.com>
2020-05-09 11:48:33 -0300

    README.md changes and other init.sh changes

--- README.md
+++ README.md
@@ -2,21 +2,22 @@
 dotfiles for my computer, don't repeat yourself, do it once do it well
 
 ## WHAT WILL BE INSTALLED
-
+ 
+  - Update System
+  - SNAP
+  - Git
+  - curl
   - Google Chrome
   - Rambox
   - Spotify
   - curl
   - git
   - zsh
-    - zsh-sintax-highliting
-  - tmux
-    - all my tmux configurations
   - vi
-    - all vi pluggins that I use
+    - all vi pluggins and alias that I use
   - rvm
-  - go
-  - python alias
-    - pip
-  - silversearch
   - Go
+
+## TO RUN
+
+sh -c "`curl -fSs  https://raw.githubusercontent.com/marcospedro97/dotfiles/master/init.sh`"
--- init.sh
+++ init.sh
@@ -3,6 +3,16 @@
 sudo pacman -Syu
 
 echo "======================================================"
+echo "Installing SNAP, CURL and Git."
+echo "======================================================"
+echo
+
+sudo pacman -S git
+sudo pacman -S curl
+sudo pacman -S snapd
+sudo systemctl enable --now snapd.socket
+
+echo "======================================================"
 echo "Installing Google Chrome."
 echo "======================================================"
 echo
@@ -11,18 +21,25 @@ git clone https://aur.archlinux.org/google-chrome.git
 cd google-chrome/
 makepkg -s
 sudo pacman -U --noconfirm google-chrome-70.0.3538.77-1-x86_64.pkg.tar.xz
+cd ..
 
-#echo "======================================================"
-#echo "Installing Spotify."
-#echo "======================================================"
-#echo
+echo "======================================================"
+echo "Installing Spotify."
+echo "======================================================"
+echo
 
-#echo "======================================================"
-#echo "Installing Rambox"
-#echo "======================================================"
-#echo
+git clone https://aur.archlinux.org/spotify.git
+cd spotify/
+makepkg -s
+sudo pacman -U --noconfirm spotify-1.0.92.390-1-x86_64.pkg.tar.xz
+cd ..
 
+echo "======================================================"
+echo "Installing Rambox"
+echo "======================================================"
+echo
 
+sudo snap install rambox
 
 echo "======================================================"
 echo "Installing ZSH"
@@ -31,14 +48,15 @@ echo
 
 cp zshrc $HOME/.vimrc
 
-
 echo "======================================================"
 echo "Configuring VIM"
 echo "======================================================"
 echo
 
+sudo pacman -S vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp vimrc $HOME/.vimrc
+vim +PlugInstall +qall
 
 echo "======================================================"
 echo "Installing RVM"
